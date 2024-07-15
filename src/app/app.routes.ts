0849579bca3b15a93a4f13df4c506f4fad775b1e
Andre Vitor Faria Ribeiro <andrev@motorola.com>
2024-07-15 12:30:45 -0300

    ajuste de lazy loading para rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { CreateDeckComponent } from './pages/create-deck/create-deck.component';
-import { DeckListComponent } from './pages/deck-list/deck-list.component';
-import { DeckDetailsComponent } from './pages/deck-details/deck-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'create', component: CreateDeckComponent },
-  { path: 'deck-list', component: DeckListComponent },
-  { path: 'deck-details/:id', component: DeckDetailsComponent },
-  { path: '**', redirectTo: '/home' },
+  {
+    path: 'home', 
+    loadComponent: () => import('./pages/home/home.component').then((c) => c.HomeComponent)
+  },
+  {
+    path: 'create', 
+    loadComponent: () => 
+      import('./pages/create-deck/create-deck.component').then((c) => c.CreateDeckComponent)
+  },
+  {
+    path: 'deck-list', 
+    loadComponent: () => 
+      import('./pages/deck-list/deck-list.component').then((c) => c.DeckListComponent)
+  },
+  {
+    path: 'deck-details/:id', 
+    loadComponent: () => 
+      import('./pages/deck-details/deck-details.component').then((c) => c.DeckDetailsComponent)
+  },
 ];
